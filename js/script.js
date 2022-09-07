4703139ceddc0ce750e7deb6620af4a565aa38c9
Daria <101666130+DariaChernomorets@users.noreply.github.com>
2022-09-07 19:06:10 +0300

    1st try (2 options)

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
+// (function () {
+//     'use strict'
+//
+//     const generateList = array => {
+//         const ul = document.createElement('ul')
+//
+//         array.forEach(elem => {
+//             const li = document.createElement('li');
+//             if (!Array.isArray(elem)) {
+//                 li.innerText = elem;
+//             } else {
+//                 li.appendChild(generateList(elem));
+//             }
+//             ul.appendChild(li)
+//         })
+//         return document.body.appendChild(ul)
+//     }
+//     generateList( [1,2, [1.1,1.2,1.3] ,3])
+// })()
+
 (function () {
     'use strict'
+const data = [1,2, [1.1, 1.2, 1.3] ,3];
+    const generateList2 = array => {
 
-    const generateList = array => {
         const ul = document.createElement('ul')
 
-        array.forEach(elem => {
+        for ( let i = 0; i <array.length; i +=1 ) {
             const li = document.createElement('li');
-            if (!Array.isArray(elem)) {
-                li.innerText = elem;
+
+            if (Array.isArray(array[i])){
+                li.append(generateList2(array[i]))
+
             } else {
-                li.appendChild(generateList(elem));
+                li.innerHTML = array[i];
             }
-            ul.appendChild(li)
-        })
-        return document.body.appendChild(ul)
+
+           ul.append(li)
+        }
+
+
+     return ul;
     }
-    generateList( [1,2, [1.1,1.2,1.3] ,3])
+
+    const list = generateList2(data)
+    document.body.append(list)
 })()
